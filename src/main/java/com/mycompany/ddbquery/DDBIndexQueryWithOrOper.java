0315adc2443fb6b9816bd212d1afe861782bc57f
Naveen Gupta <naveeng3074@gmail.com>
2021-12-13 23:49:17 +0530

    Initial release
    Initial release

---
+++ src/main/java/com/mycompany/ddbquery/DDBIndexQueryWithOrOper.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ddbquery;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Index;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Naveen
+ */
+public class DDBIndexQueryWithOrOper {
+    public static void main(String args[]){
+        try{
+            DynamoDB dynamoDB;
+            dynamoDB = new DynamoDB(new AmazonDynamoDBClient(new ProfileCredentialsProvider()));
+
+        Table table = dynamoDB.getTable("CountryInfo");
+        Index index = table.getIndex("country_name-index");
+
+        QuerySpec spec = new QuerySpec()
+            .withKeyConditionExpression("country_name = :country_name")
+            .withFilterExpression("city = :city_name_1 or city = :city_name_2")
+            .withValueMap(new ValueMap()
+                .withString(":country_name","United States")
+                .withString(":city_name_1", "West Lafayette")
+                .withString(":city_name_2", "Higbee"));
+
+        ItemCollection<QueryOutcome> items = index.query(spec);
+        Iterator<Item> iter = items.iterator(); 
+        while (iter.hasNext()) {
+            System.out.println(iter.next().toJSONPretty());
+        }
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
+    }
+}
